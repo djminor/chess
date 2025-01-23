b8b33f89603f28b0e2f635745db6d9fd1475d925
djsproul <djminor@byu.edu>
2025-01-23 00:26:45 -0700

    Implemented pieceMoves function, will implement all helper functions next

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 /**
  * Represents a single chess piece
@@ -52,6 +54,51 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        ChessPiece piece = board.getPiece(myPosition);
+        if (piece == null) {
+            return Collections.emptyList();
+        }
+
+        Collection<ChessMove> moveList = new ArrayList<>();
+
+        switch (piece.getPieceType()) {
+            case PAWN:
+                calculatePawnMoves(board, myPosition, piece, moveList);
+                break;
+            case ROOK:
+                calculateSlidingPieceMoves(board, myPosition, piece, moveList, new int[][] {{1, 0}, {0, 1}, {-1, 0}, {0, -1}});
+                break;
+            case BISHOP:
+                calculateSlidingPieceMoves(board, myPosition, piece, moveList, new int[][] {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}});
+                break;
+            case QUEEN:
+                calculateSlidingPieceMoves(board, myPosition, piece, moveList, new int[][] {
+                        {1, 1}, {1, -1}, {-1, 1}, {-1, -1}, {1, 0}, {0, 1}, {-1, 0}, {0, -1}
+                });
+                break;
+            case KNIGHT:
+                calculateKnightMoves(board, myPosition, piece, moveList);
+                break;
+            case KING:
+                calculateKingMoves(board, myPosition, piece, moveList);
+        };
+        return moveList;
+    }
+
+    private void calculatePawnMoves(ChessBoard board, ChessPosition position, ChessPiece piece, Collection<ChessMove> moves) {
+        int moveDirection = piece.getTeamColor() == ChessGame.TeamColor.WHITE ? 1 : -1;
+        int row = position.getRow();
+        int col = position.getColumn();
+        ChessPosition forward = new ChessPosition(col, row + moveDirection);
+        moves.add(new ChessMove(position, forward, getPieceType()));
+    }
+    private void calculateSlidingPieceMoves(ChessBoard board, ChessPosition position, ChessPiece piece, Collection<ChessMove> moves, int[][] directions) {
+
+    }
+    private void calculateKnightMoves(ChessBoard board, ChessPosition position, ChessPiece piece, Collection<ChessMove> moves) {
+
+    }
+    private void calculateKingMoves(ChessBoard board, ChessPosition position, ChessPiece piece, Collection<ChessMove> moves) {
+
     }
 }
