06932e73c8146f417deda84372c660e48132158d
djsproul <djminor@byu.edu>
2025-03-02 02:02:50 -0700

    8 of 21 tests passing, login and register completely implemented

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -31,6 +31,15 @@ public class Server {
 
     private Object register(Request request, Response response) {
         JsonObject reqJson = gson.fromJson(request.body(), JsonObject.class);
+        if (!reqJson.has("username") || reqJson.get("username").isJsonNull() || reqJson.get("username").getAsString().isEmpty() ||
+                !reqJson.has("password") || reqJson.get("password").isJsonNull() || reqJson.get("password").getAsString().isEmpty() ||
+                !reqJson.has("email") || reqJson.get("email").isJsonNull() || reqJson.get("email").getAsString().isEmpty()) {
+
+            JsonObject json = new JsonObject();
+            json.addProperty("message", "Error: Bad Request");
+            response.status(400);
+            return gson.toJson(json);
+        }
         String username = reqJson.get("username").getAsString();
         String password = reqJson.get("password").getAsString();
         String email = reqJson.get("email").getAsString();
@@ -40,13 +49,17 @@ public class Server {
         if (userData == null) {
             service.createUser(username, password, email);
             authToken = service.createAuth(username);
+            JsonObject json = new JsonObject();
+            json.addProperty("username", username);
+            json.addProperty("authToken", authToken);
+            response.status(200);
+            return gson.toJson(json);
+        } else {
+            JsonObject json = new JsonObject();
+            json.addProperty("message", "Error: Forbidden");
+            response.status(403);
+            return gson.toJson(json);
         }
-
-        JsonObject json = new JsonObject();
-        json.addProperty("username", username);
-        json.addProperty("authToken", authToken);
-        response.status(200);
-        return gson.toJson(json);
     }
 
     private Object login(Request request, Response response) {
@@ -54,9 +67,11 @@ public class Server {
         String username = reqJson.get("username").getAsString();
         String password = reqJson.get("password").getAsString();
 
-        if (username == null || username.isEmpty()) {
-            response.status(400);
-            return "{\"error\": \"Username is required\"}";
+        if (service.getUser(username) == null) {
+            JsonObject errorJson = new JsonObject();
+            errorJson.addProperty("message", "Error: Unauthorized");
+            response.status(401);
+            return gson.toJson(errorJson);
         }
 
         if (verifyPassword(username, password)) {
@@ -67,8 +82,10 @@ public class Server {
             response.status(200);
             return gson.toJson(json);
         } else {
+            JsonObject errorJson = new JsonObject();
+            errorJson.addProperty("message", "Error: Unauthorized");
             response.status(401);
-            return "{\"error\": \"Password is incorrect\"}";
+            return gson.toJson(errorJson);
         }
     }
 
