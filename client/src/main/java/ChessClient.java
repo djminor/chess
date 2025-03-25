575ba63ad90622ad39a5799277d3d47675a8af46
djsproul <djminor@byu.edu>
2025-03-24 21:17:50 -0600

    Implemented input loop/help and quit commands

--- client/src/main/java/ChessClient.java
+++ client/src/main/java/ChessClient.java
@@ -1,5 +1,46 @@
+import ui.EscapeSequences;
+
+import java.util.Scanner;
+
 public class ChessClient {
+    private final Scanner scanner = new Scanner(System.in);
+
     public void run() {
         System.out.print("♕ Welcome to 240 chess. Type 'help' to get started ♕");
+        boolean running = true;
+        while (running) {
+            System.out.print("\n[LOGGED OUT] >>>");
+            String input = scanner.nextLine().trim();
+            if (input.equals("help")) {
+                printHelp();
+            }
+            if (input.equals("quit")) {
+                System.out.print( EscapeSequences.SET_TEXT_COLOR_YELLOW + "See you soon!");
+                running = false;
+            }
+        }
+    }
+
+    public void printHelp() {
+        System.out.print(EscapeSequences.SET_TEXT_COLOR_BLUE +
+                "register <USERNAME> <PASSWORD> <EMAIL>" +
+                EscapeSequences.RESET_TEXT_COLOR +
+                " - to create an account\n"
+        );
+        System.out.print(EscapeSequences.SET_TEXT_COLOR_BLUE +
+                "login <USERNAME> <PASSWORD>" +
+                EscapeSequences.RESET_TEXT_COLOR +
+                " - to play chess\n"
+        );
+        System.out.print(EscapeSequences.SET_TEXT_COLOR_BLUE +
+                "quit" +
+                EscapeSequences.RESET_TEXT_COLOR +
+                " - playing chess\n"
+        );
+        System.out.print(EscapeSequences.SET_TEXT_COLOR_BLUE +
+                "help" +
+                EscapeSequences.RESET_TEXT_COLOR +
+                " - with possible commands"
+        );
     }
 }
